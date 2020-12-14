0be94b03ae5b4878423834733db112a0e643dee5
Artur Signell <artur@vaadin.com>
2020-12-14 13:41:13 +0200

    Upgrade to Vaadin 19 and Flow offline feature snapshot

--- src/main/java/com/example/app/Application.java
+++ src/main/java/com/example/app/Application.java
@@ -1,14 +1,19 @@
 package com.example.app;
 
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.vaadin.artur.helpers.LaunchUtil;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+@PWA(name = "Offline Form", shortName = "Offline Form"/* , offlineResources = { "**" } */)
+public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        LaunchUtil.launchBrowserInDevelopmentMode(SpringApplication.run(Application.class, args));
     }
 
 }
